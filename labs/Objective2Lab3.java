5bbd5fc5fffb17b2e88043797f2d9bd46b487b2b
Tyson Arellano <tyson.arellano@yahoo.com>
2024-09-30 12:24:23 -0600

    Objevtive2Lab3

---
+++ labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+    public static void main(String[] args) {
+        int birthYear = 1989;
+        String month = "September";
+        Boolean isWeekend = false;
+        System.out.println(birthYear);
+        System.out.println(month);
+        System.out.println(isWeekend);
+    }
+  }
