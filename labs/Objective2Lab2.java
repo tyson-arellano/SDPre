a3c2c2196bbfff6ce333108b73416bd8b4926c76
Tyson Arellano <tyson.arellano@yahoo.com>
2024-09-30 12:12:45 -0600

    Objective2Lab2

---
+++ labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+    public static void main(String[] args) {
+      String name = "Tyson Arellano";
+      int age = 35;
+      String state = "Colorado";
+  
+      System.out.println("Name: " + name);
+      System.out.println("Age: " + age);
+      System.out.println("State: " + state);
+    }
+  }
