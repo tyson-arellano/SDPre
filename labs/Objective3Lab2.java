c769329ac5a17bafe029190e08c6bb512d5c26ff
Tyson Arellano <tyson.arellano@yahoo.com>
2024-10-01 12:59:42 -0600

    Objective3Lab2

---
+++ labs/Objective3Lab2.java
@@ -0,0 +1,8 @@
+public class Objective3Lab2 {
+    public static void main(String[] args) {
+        double side1 = 8;
+        double side2 = 10;
+        double hypotenuse = Math.sqrt((side1 * side1) + (side2 * side2));
+        System.out.println(hypotenuse);
+    }
+}
