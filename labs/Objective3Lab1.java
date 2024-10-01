81c62cc95c71903fe100295a5ba93225b011f0f8
Tyson Arellano <tyson.arellano@yahoo.com>
2024-10-01 12:40:07 -0600

    Objective3Lab1

---
+++ labs/Objective3Lab1.java
@@ -0,0 +1,12 @@
+
+    public class Objective3Lab1 {
+        public static void main(String[] args) {
+          double num1 = 1;
+          double num2 = 2;
+          double num3 = 3;
+          double average = (num1 + num2 + num3) / 3;
+          System.out.println("The average is: " + average);
+      
+        }
+      }
+
