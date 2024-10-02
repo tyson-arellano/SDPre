fe9a82680c1a4069f253d646c6529af3abe876cb
Tyson Arellano <tyson.arellano@yahoo.com>
2024-10-02 12:13:27 -0600

    Objective4Lab1

---
+++ labs/Objective4Lab1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Objective4Lab1 {
+  public static void main(String[] args) {
+
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("Please enter your first name");
+    String fname = keyboard.nextLine();
+    System.out.println("Please enter your last name");
+    String lname = keyboard.nextLine();
+    System.out.println("Please enter your favorite animal");
+    String favoriteAnimal = keyboard.nextLine();
+    System.out.println("Please enter your favorite food");
+    String favoriteFood = keyboard.nextLine();
+    System.out.println("Please enter your favorite song");
+    String favoriteSong = keyboard.nextLine();
+
+
+    System.out.println("My name is " +  fname  + " " + lname);
+    System.out.println("My favorite animal is " +  favoriteAnimal);
+    System.out.println("My favorite food is " +  favoriteFood);
+    System.out.println("My favorite song is " +  favoriteSong);
+
+    
+  }
+}
