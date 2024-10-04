30c4298d8262eca4dbfc39d515fb65f65fd4cf09
Tyson Arellano <tyson.arellano@yahoo.com>
2024-10-04 13:11:11 -0600

    Objective6Llab1

---
+++ labs/Objective6Lab1.java
@@ -0,0 +1,46 @@
+public class Objective6Lab1 {
+    public static void main(String[] args) {
+      int month = 6;
+  
+      switch (month) {
+        case 1:
+          System.out.println("January");
+          break;
+        case 2:
+          System.out.println("February");
+          break;
+        case 3:
+          System.out.println("March");
+          break;
+         case 4:
+          System.out.println("April");
+          break;
+        case 5:
+          System.out.println("May");
+          break;
+        case 6:
+          System.out.println("June");
+          break;
+       
+        default:
+          System.out.println("That is not a valid month");
+          break;
+      }
+    }
+  }
+// public class Objective6Lab1 {
+//     public static void main(String[] args) {
+//       int month = 6;
+  
+//       switch (month) {
+//         case 1 -> System.out.println("January");
+//         case 2 -> System.out.println("February");
+//         case 3 -> System.out.println("March");
+//          case 4 -> System.out.println("April");
+//         case 5 -> System.out.println("May");
+//         case 6 -> System.out.println("June");
+       
+//         default -> System.out.println("That is not a valid month");
+//       }
+//     }
+//   }
