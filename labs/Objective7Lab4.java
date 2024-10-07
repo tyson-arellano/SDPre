43d9632efafd69cf30d3afe0839fce788e2e3951
Tyson Arellano <tyson.arellano@yahoo.com>
2024-10-07 11:50:51 -0600

    Objective7Lab4

---
+++ labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+public class Objective7Lab4 {
+    public static void main(String[] args) {
+        int count = 1;
+        int sum = 0;
+
+        while(count <= 20){
+            sum += count;
+            count++;
+            
+        }
+        System.out.println(sum);
+    }
+    
+}
